afca23790a064b3e59bfd9c505cfa9ed73eec894
Hakan Gurbuz <hakangurbuz20@gmail.com>
2021-01-16 22:07:49 +0300

    Owner repo and entity added
    This entity has a one to many relation with car entity

---
+++ src/main/java/com/hgurbuz/carapp/Repository/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.hgurbuz.carapp.Repository;
+
+import com.hgurbuz.carapp.Entity.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner,Long> {
+}
