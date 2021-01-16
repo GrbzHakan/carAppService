14b8a8e4a8920b9b1687c85a623250453e925428
Hakan Gurbuz <hakangurbuz20@gmail.com>
2021-01-17 00:25:57 +0300

    CarRepository updated
    To add custom methods in to the api which produced before  with using data-jpa

--- src/main/java/com/hgurbuz/carapp/Repository/CarRepository.java
+++ src/main/java/com/hgurbuz/carapp/Repository/CarRepository.java
@@ -2,6 +2,16 @@ package com.hgurbuz.carapp.Repository;
 
 import com.hgurbuz.carapp.Entity.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car,Long> {
+    //Fetch cars by brand
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    //Fetch cars by color
+    List<Car> findByColor(@Param("color") String color);
 }
