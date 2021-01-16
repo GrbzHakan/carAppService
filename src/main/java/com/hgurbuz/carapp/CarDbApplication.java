08803c694d8ff17b1300c47da0e749049a978699
Hakan Gurbuz <hakangurbuz20@gmail.com>
2021-01-16 22:44:06 +0300

    Update CarDbApplication.java
    Owner demo data added for test

--- src/main/java/com/hgurbuz/carapp/CarDbApplication.java
+++ src/main/java/com/hgurbuz/carapp/CarDbApplication.java
@@ -1,7 +1,9 @@
 package com.hgurbuz.carapp;
 
 import com.hgurbuz.carapp.Entity.Car;
+import com.hgurbuz.carapp.Entity.Owner;
 import com.hgurbuz.carapp.Repository.CarRepository;
+import com.hgurbuz.carapp.Repository.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +14,9 @@ import org.springframework.context.annotation.Bean;
 public class CarDbApplication {
 	@Autowired
 	private CarRepository carRepository;
-
+	@Autowired
+	private OwnerRepository ownerRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CarDbApplication.class, args);
 	}
@@ -20,13 +24,19 @@ public class CarDbApplication {
 	@Bean
 	CommandLineRunner runner(){
 		return args -> {
+
+			Owner owner1 = new Owner("Hakan","Gurbuz");
+			Owner owner2 = new Owner("Test","Mest");
+
+			ownerRepository.save(owner1);
+			ownerRepository.save(owner2);
 			// Save demo data to db
 			carRepository.save(new Car("Ford", "Mustang", "Red",
-					"ADF-1121", 2017, 59000));
+					"ADF-1121", 2017, 59000,owner1));
 			carRepository.save(new Car("Nissan", "Leaf", "White",
-					"SSJ-3002", 2014, 29000));
+					"SSJ-3002", 2014, 29000,owner2));
 			carRepository.save(new Car("Toyota", "Prius", "Silver",
-					"KKO-0212", 2018, 39000));
+					"KKO-0212", 2018, 39000,owner1));
 		};
 	}
 
