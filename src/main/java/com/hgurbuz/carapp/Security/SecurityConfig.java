2eca6b819b091f75e44a0346ba3136c47240190f
Hakan Gurbuz <hakangurbuz20@gmail.com>
2021-01-17 19:04:05 +0300

    Login and other filter config

--- src/main/java/com/hgurbuz/carapp/Security/SecurityConfig.java
+++ src/main/java/com/hgurbuz/carapp/Security/SecurityConfig.java
@@ -1,14 +1,18 @@
-package com.hgurbuz.carapp.Configuration;
+package com.hgurbuz.carapp.Security;
 
+import com.hgurbuz.carapp.Security.Filter.AuthenticationFilter;
+import com.hgurbuz.carapp.Security.Filter.LoginFilter;
 import com.hgurbuz.carapp.Service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +24,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // Filter for the api/login requests
+                .addFilterBefore(new LoginFilter("/login",
+                                authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // Filter for other requests to check JWT in header
+                .addFilterBefore(new AuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 }
